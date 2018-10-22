d9a289aa7b17137fe952d2f8675f2497a995a157
jtuburan <jasontuburan@gmail.com>
2018-10-22 09:46:21 +0800

    user model created

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,7 +5,6 @@ const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
-
 userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("user",userSchema);
