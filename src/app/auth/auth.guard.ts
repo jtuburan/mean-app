d97f6f558484749e832cbfb988afd3ce687eddd8
jason <jasontuburan@gmail.com>
2018-11-14 15:15:59 +0800

    token expiration in UI

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -5,7 +5,7 @@ import {
         Router} from "@angular/router";
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
-import { AuthService } from "./signup/auth.service";
+import { AuthService } from "./auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AuthData } from '../auth-data.model';
+import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
 
@@ -11,7 +11,8 @@ export class AuthService {
   private isAuthenticated = false;
   private token: string;
   private authStatusListener = new Subject<boolean>();
-
+  private tokenTimer: any;
+  
   constructor(private http: HttpClient, private router: Router) { }
 
   getToken() {
@@ -36,11 +37,15 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = {email: email, password: password}
-    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string, expiresIn: number}>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
           const token = response.token;
           this.token = token;
           if(token) {
+            const expiresInDuration = response.expiresIn;
+            this.tokenTimer = setTimeout(() => {
+              this.logout();
+            }, expiresInDuration );
             this.isAuthenticated = true;
             this.authStatusListener.next(true);
             this.router.navigate(['/']);
@@ -53,6 +58,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
     this.router.navigate(['/']);
   }
 
